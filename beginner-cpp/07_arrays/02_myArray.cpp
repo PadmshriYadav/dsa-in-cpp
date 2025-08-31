105ded0d34752378e99ae86003ba558501efdbb6
PadmshriYadav <yadavpadmshri@gmail.com>
2025-08-31 22:42:20 +0530

    aeeay

--- beginner-cpp/07_arrays/02_myArray.cpp
+++ beginner-cpp/07_arrays/02_myArray.cpp
@@ -1,12 +1,36 @@
 
 #include <iostream>
+#include <string>
 
+//doesnt work
     
-function askYouColor(){
-    
-}
+// void askYouColor(string color[] , int num ){
+     
+//     for ( int i = 0 ; i< num ; i++){
+//         cout<<"the"<<i+1<<" color should be:"<<"\n";
+//         string userColor;
+//         getline(cin,userColor);
+
+//         color[i]= userColor;
+
+//     }
+  
+// }
 using namespace std;
-int main (){
+int main(){
+//     int num;
+//     cout << "enter  no of ur color :\n";
+//     cin>>num;
 
+//    string name [num];
+
+//     askYouColor(name,num);
+
+//     cout << "\nYour colors are:\n";
+//     for (int i = 0; i < num.size(); i++) {
+//         cout << name[i] << "\n";
+//     }
     return 0;
 }
+
+
---
+++ beginner-cpp/07_arrays/03_myArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+
+    // int number[]={1,12,3,24,5};
+    // //to find index of array 24
+    // int size = sizeof(number) / sizeof(number[0]); //memory size20/4
+    // for (int i=0; i< size; i++){
+    //     if( number[i] == 24){
+    //         cout<<i<<'\n';
+    //     }
+    // }
+
+    // int num ;
+    // cout<<"sum of what \n";
+    // cin>>num;
+    // int sum =0;
+
+    // for ( int i=1;i<=num;i++){
+    //     sum+= i ;
+
+    // }
+    // cout<<"the sum is"<<sum<<"\n";
+    
+    return 0;
+}
