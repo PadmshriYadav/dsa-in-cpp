8f65eda49dbc629302b96ddd7ae9ed7b9d9c3edc
PadmshriYadav <yadavpadmshri@gmail.com>
2025-09-02 19:29:56 +0530

    with parameter

---
+++ beginner-cpp/09_OOP/03_constructor.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+class Dog {
+    
+    public: // fully accessible anywhere
+
+    //data memebrs/structure (attributes or properties)
+     string name;
+     vector<string> treat;
+     int age;
+
+     // with parametr constructor
+     Dog( string dogName, vector<string> dogTreat,int dogAgw){          
+     name = dogName;
+          
+     treat =  dogTreat ;
+          
+     age = dogAgw;
+    cout<<"constructor called"<< endl;
+
+     }
+
+     //member function/behaviour(methods or functions)
+
+     void aboutDogs(){
+        cout << name << " barks like a bird !!. \n her/his age is:"<<age <<endl;
+        cout << "treats :";
+        for (string dog_treat :treat){
+            cout << dog_treat<<" ";
+        }
+
+     }
+};
+
+int main (){
+    //object
+    Dog dog1("mini",{"a","b","c"},3);
+    dog1.aboutDogs();
+
+}
