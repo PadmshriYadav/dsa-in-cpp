68b4cf8087517e6bba99985d8ad3158fbfc86add
PadmshriYadav <yadavpadmshri@gmail.com>
2025-09-08 22:06:12 +0530

    stlll

---
+++ beginner-cpp/11_mySTL/01_sTemplateLibrar.cpp
@@ -0,0 +1,23 @@
+//-----> containers as different kinds of storage boxes for your data.
+//vector,list,map
+//-----> iterator is how the algorithms know which stuff to work on
+//names.begin(), names.end()
+//----> Algorithms are the actions, a set of instructions(you write after ".")
+// sort algorithm is like a rule that says, "Put all your toys in order from smallest to biggest."
+
+
+// 1  Include Headers
+
+#include <iostream>   // For printing to the console
+#include <vector>     // For the vector container (vector is a dynamic array)
+#include <string>     // To use strings
+#include <algorithm>  // For the sort and find algorithms
+
+// 2. Containers (The Data)
+// 33. Algorithms & Iterators 
+
+
+
+// summaryyyyyy
+/*about using pre-built containers and methods 
+(which are part of algorithms and the containers themselves) to handle your data*/
